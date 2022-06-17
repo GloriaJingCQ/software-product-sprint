0248c26906d1d11e67cfefa3e032e452bca6855e
Chenqian Jing <87431812+GloriaJingCQ@users.noreply.github.com>
2022-06-17 14:08:00 -0700

    Update HelloWorldServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -6,13 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
-
+/** Handles requests sent to the /hello URL. Try running a server and navigating to /interests */
+@WebServlet("/interests")
+public class InterestServlet extends HttpServlet {
+  ArrayList<String> mylist = new ArrayList<String> ();
+  mylist.add("Hiking");
+  mylist.add("Boxing");
+  mylist.add("Salsa dance");
+  String json = new Gson().toJson(mylist);
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+    }
+
   }
-}
